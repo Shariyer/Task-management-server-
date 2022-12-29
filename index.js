b8944243c2e05f825ab6933078d8adf7125cf81d
shariyer <s.m.shariyer04@gmail.com>
2022-12-29 18:10:19 +0600

    all tasks route added

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ require("dotenv").config();
 // database connection
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { all } = require("express/lib/application");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhtiicz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -22,7 +23,19 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-app.get("/", async (req, res) => {
+async function run() {
+  const taskCollection = client
+    .db("TaskManagementDB")
+    .collection("taskCollection");
+
+  app.get("/tasks", async (req, res) => {
+    const query = {};
+    const allTasks = await taskCollection.find(query);
+    res.send(allTasks);
+  });
+}
+run().catch((err) => console.log(err));
+run.app.get("/", async (req, res) => {
   res.send("My task management project is running successfully");
 });
 app.listen(port, async (req, res) => {
